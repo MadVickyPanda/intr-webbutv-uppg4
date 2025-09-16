d78f8f9b7ce0098ee5de1ff519ed6596c1592b5f
MadVickyPanda <solboll@hotmail.com>
2025-09-16 12:14:48 +0200

    js försök 1

---
+++ script.js
@@ -0,0 +1,54 @@
+const myNodelist=document.getElementsByTagName("LI");
+let i;
+for (i=0; i <myNodelist.length; i++) {
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    span.className="close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+const close =document.getElementsByClassName("close");
+let i;
+for (i=0; i<close.length; i++) {
+    close [i].onclick = function () {
+        const div =this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+const list =document.querySelector("ul");
+list.addEventListener("click", function(ev) {
+    if (ev.target.tagname === "LI") {
+        ev.target.classList.toggle("checked")
+     }
+    }
+,false);
+
+function newElement() {
+    let li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue ==="") {
+        alert("Du måste skriva något!!!");
+    } else{
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value ="";
+
+    const span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className="close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i=0; i< close.length; i++){
+        close[i].onclick= function(){
+            let div = this.parentElement;
+            div.style.display="none";
+        }
+    }
+} 
+
+
